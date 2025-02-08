b0e7ffc831a386a7278325266d07b676dc89cb42
bhoomsiri <phumsiri.sir@gmail.com>
2025-02-08 08:40:07 +0000

    Creat DAGs and Modify Compose

--- dags/weather_api_dag.py
+++ dags/weather_api_dag.py
@@ -1,15 +1,22 @@
+import json
+from datetime import timedelta
+
 from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.email import EmailOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils import timezone
 
 import requests
 
+dags_path = "/opt/airflow/dags/data.json"
 
 def _get_weather_data():
-    API_KEY = "793898b2170c25fc2734549646c162f2"
+    #assert 1==2
     # API_KEY = os.environ.get("WEATHER_API_KEY")
-    # API_KEY = Variable.get("weather_api_key")
+    API_KEY = Variable.get("weather_api_key")
 
     payload = {
         "q": "bangkok",
@@ -23,12 +30,66 @@ def _get_weather_data():
     data = response.json()
     print(data)
 
+    with open("dags_path", "w") as f:
+        json.dump(data, f)
 
+
+def _validate_data():
+    with open("dags_path", "r") as f:
+        data = json.load(f)
+
+    assert data.get("main") is not None
+
+
+def _create_weather_table():
+    pg_hook = PostgresHook(
+        postgres_conn_id="weather_postgres_conn",
+        schema="postgres"
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    sql = """
+        CREATE TABLE IF NOT EXISTS weathers (
+            dt BIGINT NOT NULL,
+            temp FLOAT NOT NULL
+        )
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+
+def _load_data_to_postgres():
+    pg_hook = PostgresHook(
+        postgres_conn_id="weather_postgres_conn",
+        schema="postgres"
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    with open("dags_path", "r") as f:
+        data = json.load(f)
+
+    temp = data["main"]["temp"]
+    dt = data["dt"]
+    sql = f"""
+        INSERT INTO weathers (dt, temp) VALUES ({dt}, {temp})
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+
+default_args = {
+    "email": ["67130113@dpu.ac.th"],
+    "retries": 3,
+    "retry_delay": timedelta(minutes=1),
+}
 with DAG(
     "weather_api_dag",
+    default_args=default_args,
     schedule="0 */3 * * *",
     start_date=timezone.datetime(2025, 2, 1),
-    tags=["dpu"]
+    tags=["dpu"],
 ):
     start = EmptyOperator(task_id="start")
 
@@ -37,6 +98,30 @@ with DAG(
         python_callable=_get_weather_data,
     )
 
+    validate_data = PythonOperator(
+        task_id="validate_data",
+        python_callable=_validate_data,
+    )
+
+    create_weather_table = PythonOperator(
+        task_id="create_weather_table",
+        python_callable=_create_weather_table,
+    )
+
+    load_data_to_postgres = PythonOperator(
+        task_id="load_data_to_postgres",
+        python_callable=_load_data_to_postgres,
+    )
+
+    send_email = EmailOperator(
+        task_id="send_email",
+        to=["67130113@dpu.ac.th"],
+        subject="Finished getting open weather data",
+        html_content="Done",
+    )
+
     end = EmptyOperator(task_id="end")
 
-    start >> get_weather_data >> end
+    start >> get_weather_data >> validate_data >> load_data_to_postgres >> send_email
+    start >> create_weather_table >> load_data_to_postgres
+    send_email >> end
